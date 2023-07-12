9e32d2449fdc370a48acb78c07f8a3236142cc64
MKBABA01 <138124118+MKBABA01@users.noreply.github.com>
2023-07-13 01:36:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ class Config(object):
     START_PIC   = os.environ.get("START_PIC", "")
     ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '5497597228').split()]
     FORCE_SUB   = os.environ.get("FORCE_SUB", "t.me/hellomerijaanm") 
-    
+    LOG_CHANNEL = os.environ.get("LOG_CHANNEL", -1001946640368")
 
     # wes response configuration     
     WEBHOOK = bool(os.environ.get("WEBHOOK", True))
